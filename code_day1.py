aa404c4ec5b4b6d84b064ee038f45bfc948f4b6f
codewithsrushti <ssrushtich@gmail.com>
2026-03-24 19:13:44 +0530

    Create code_day1.py

---
+++ code_day1.py
@@ -0,0 +1,15 @@
+# Day 1 - Basics
+print("Hello, Github!")
+
+#Variables
+name = "Srushti"  #string
+age = 20  #integer
+
+#Output
+print("Name:", name)
+print("Age:", age)
+
+# Expected Output:
+# Hello Github!
+# Name: Srushti
+# Age: 20
