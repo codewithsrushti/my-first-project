84d5ce7bf4ee4257c214b5d55b260ac381406c0e
Srushti C Hiremath <ssrushtich@gmail.com>
2026-03-25 19:57:46 +0530

    Update code_day2.py

--- code_day2.py
+++ code_day2.py
@@ -1,26 +1,40 @@
-# Day 2 - If Else
+# Day 2 - Python If,Else,Elif Statements
 
-# Example 1
+# Example 1 - Greater than 
 num = 10
 if num > 5:
   print("Number is greater than 5")
 else:
   print("Number is less than or equal to 5")
 
-# Example 2
+# Example 2 - Even or Odd
 num = 7
 if num % 2 == 0:
   print("The number is even")
 else:
   print("The number is odd")
   
-# Example 3 - Positive or Negative using If Else
+# Example 3 - Positive/Neagtive/Zero
 
 num = int(input("Enter a number:"))
 if num > 0:
   print("Number is positive")
+elif num == 0:
+  print("Number is Zero")
 else:
   print("Number is negative")
+
+# Example 4 - Largest of Two Numbers 
+
+a = int(input("Enter First Number:"))
+b = int(input("Enter the Second Number:"))
+
+if a > b:
+  print("First number is greater")
+elif a == b:
+  print("Both numbers are equal")
+else:
+  print("Second number is greater")
 
 
 # Expected Output
@@ -32,3 +46,9 @@ else:
 # Expected Output
 # Enter a number: -3
 # Number is negative
+
+# Expected Output
+# Enter First Number:10
+# Enter the Second Number:20
+# Second number is greater
+
