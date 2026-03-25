4eb6b29db2449a734817dd51d5dc608860b06fa8
Srushti C Hiremath <ssrushtich@gmail.com>
2026-03-25 19:02:31 +0530

    Update code_day1.py

--- code_day1.py
+++ code_day1.py
@@ -1,15 +1,15 @@
 # Day 1 - Basics
 print("Hello, Github!")
 
-#Variables
+# Variables
 name = "Srushti"  #string
 age = 20  #integer
 
-#Output
+# Output
 print("Name:", name)
 print("Age:", age)
 
-# Expected Output:
+# Expected Output
 # Hello Github!
 # Name: Srushti
 # Age: 20
