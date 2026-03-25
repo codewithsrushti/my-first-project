7e74950543193b73c855a8991c9022ce218b3923
Srushti C Hiremath <ssrushtich@gmail.com>
2026-03-25 19:42:37 +0530

    Update code_day2.py

--- code_day2.py
+++ code_day2.py
@@ -13,7 +13,22 @@ if num % 2 == 0:
   print("The number is even")
 else:
   print("The number is odd")
+  
+# Example 3 - Positive or Negative using If Else
+
+num = int(input("Enter a number:"))
+if num > 0:
+  print("Number is positive")
+else:
+  print("Number is negative")
+
 
 # Expected Output
 # Number is greater than 5
+
+Expected Output
 # The number is odd
+
+# Expected Output
+# Enter a number: -3
+# Number is negative
