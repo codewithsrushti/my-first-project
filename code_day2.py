a0bd152258603cc4841d6f10df84ed2e0a7fc937
Srushti C Hiremath <ssrushtich@gmail.com>
2026-03-25 18:58:19 +0530

    Create code_day2.py

---
+++ code_day2.py
@@ -0,0 +1,21 @@
+# Day 2 - If Else
+
+# Example 1
+num = 10
+
+if num > 5:
+  print("Number is greater than 5")
+else:
+  print("Number is less than or equal to 5")
+
+# Exmaple 2
+num = 7
+
+if num % 2 == 0:
+  print("The number is even")
+else:
+  print("The number is odd")
+
+# Expected Output
+# Number is greater than 5
+# The number is odd
