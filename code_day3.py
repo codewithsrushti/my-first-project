f000559b1707ce69c9709b2174950131694008d7
Srushti C Hiremath <ssrushtich@gmail.com>
2026-03-26 20:42:03 +0530

    Update code_day3.py

--- code_day3.py
+++ code_day3.py
@@ -4,26 +4,26 @@ def function_name(parameters):
   # code
   return value
 
-# Example 1
+# Example 1 (Simple Function)
 def greet():
   print("Hello!")
 
 greet()
 
-# Example 2
+# Example 2 (Function with Parameter)
 def greet(name):
   print("Hello",name)
 
 greet("Srushti")
 
-# Example 3
+# Example 3 (Function with Return Value)
 def add(a,b):
   return a + b
 
 result = add(5,3)
 print(result)
 
-# Example 4
+# Example 4 (Real Simple Practice)
 def even_odd(n):
   if n % 2 == 0:
     return "Even"
