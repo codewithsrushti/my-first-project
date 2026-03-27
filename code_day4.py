fa73f213dbbfea2a090db204cec5fdc70e6bbf2b
Srushti C Hiremath <ssrushtich@gmail.com>
2026-03-27 21:32:24 +0530

    Create code_day4.py

---
+++ code_day4.py
@@ -0,0 +1,40 @@
+# Day - 4 Lists
+# List is a collection of values/items stored in one variable
+# Syntax
+list_name = [item1,item2,item3]
+
+# Example 1 : Access Elements
+numbers = [10,20,30]
+print(numbers[0]) # first element
+print(numbers[1]) # Second element
+
+# Example 2 : Add elements
+numbers = [1,2,3]
+numbers.append(4)
+print(numbers)
+
+# Example 3 : Loop through List
+numbers = [10,20,30]
+
+for num in numbers:
+  print(num)
+  
+# Example 4 : Find Sum of list
+numbers = [1,2,3,4]
+total = 0
+for num in numbers:
+  total += num
+
+print("Sum=",total)
+
+# Expected Output
+# 10
+# 20
+
+# [1,2,3,4]
+
+# 10
+# 20
+# 30
+
+# Sum = 10
