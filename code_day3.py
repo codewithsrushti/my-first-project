33e8b7d8d04ca3d0904e2ef95b3ddc167f2e3488
Srushti C Hiremath <ssrushtich@gmail.com>
2026-03-27 21:39:32 +0530

    Update code_day3.py

--- code_day3.py
+++ code_day3.py
@@ -1,30 +1,32 @@
 # Day 3 - Functions
+# Function is a block of code used to perform specific task.
+# It runs only when you call it.
 
 # Syntax
 def function_name(parameters):
   # code
   return value
 
-# Example 1 (Simple Function)
+# Example 1 Simple Function
 def greet():
   print("Hello!")
 
 greet()
 
-# Example 2 (Function with Parameter)
+# Example 2 Function with Parameter
 def greet(name):
   print("Hello",name)
 
 greet("Srushti")
 
-# Example 3 (Function with Return Value)
+# Example 3 Function with Return Value
 def add(a,b):
   return a + b
 
 result = add(5,3)
 print(result)
 
-# Example 4 (Real Simple Practice)
+# Example 4 Real Simple Practice
 def even_odd(n):
   if n % 2 == 0:
     return "Even"
@@ -34,6 +36,7 @@ def even_odd(n):
 print(even_odd(7))
 
 # Expected Output
+
 # Hello!
 # Hello Srushti
 # 8
